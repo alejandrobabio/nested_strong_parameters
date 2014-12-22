238bdeb45752e2089e06935f1d54a42c6b5f1484
Alejandro Babio <alejandro.e.babio@hotmail.com>
2014-12-22 18:32:27 -0300

    include nested_strong_parameters to active_record

--- lib/nested_strong_parameters.rb
+++ lib/nested_strong_parameters.rb
@@ -1,5 +1,4 @@
 require "nested_strong_parameters/version"
 require "nested_strong_parameters/nested_strong_parameters"
 
-require "active_record"
-ActiveRecord::Base.include(NestedStrongParameters)
+require "active_record/base"
